d75f56dd17921e8711d8d1e34828d0c9be9aad28
Federico <cuervog992@gmail.com>
2022-10-05 23:44:41 -0300

    componente error y mostrarlo en pantalla

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,7 @@
 import { Form, useActionData } from 'react-router-dom'
 import Formulario from '../components/Formulario'
+import Error from '../components/Error'
+import { Children } from 'react'
 
 export async function action({request}) {
 
@@ -7,14 +9,28 @@ export async function action({request}) {
 
   const datos = Object.fromEntries(formData)
 
+  const email = formData.get('email')
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
   const errores = []
 
   if (Object.values(datos).includes ('')) {
+
     errores.push('Todos los campos son obligatorios')
+
   }
 
-  if (errores.length) {
+  if (!regex.test(email)) {
+
+    errores.push('Formato de email invalido')
+
+  }
+
+  if (Object.keys(errores).length) {
+
     return errores
+
   }
 
 }
@@ -31,8 +47,18 @@ const NuevoCliente = () => {
         <button className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'>Volver</button>
       </div>
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        {
+          errores?.length && errores.map((error, i) => (
+            <Error
+              key={i}
+            >
+              {error}
+            </Error>
+          ))
+        }
         <Form
           method='post'
+          noValidate
         >
           <Formulario />
           <input 
